ef47918244996c8bfdf5e9a16f7482e749ff5ead
CyrusWarner <cyruswarner27@gmail.com>
2021-08-03 15:03:47 -0500

    Added Front end and back end section for technologies display.

--- src/Components/Technologies/technologies.jsx
+++ src/Components/Technologies/technologies.jsx
@@ -1,16 +1,39 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import "./technology.css";
 
 const Technologies = () => {
   return (
-    <Container>
-      <Row>
-        <Col sm={8}>
-          <h1 className="title ">Technologies</h1>
-        </Col>
-        <Col sm={4}></Col>
-      </Row>
-    </Container>
+    <React.Fragment>
+      <Container>
+        <Row>
+          <Col sm={8}>
+            <h1 className="title ">Technologies</h1>
+          </Col>
+          <Col sm={4}></Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+          <Col sm={4}>
+            <h5 className="technology-title">Front-End</h5>
+          </Col>
+          <Col sm={4}>
+            <h5 className="technology-title">Back-End</h5>
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+          <Col sm={4}>
+            <p >React</p>
+          </Col>
+          <Col sm={4}>
+            <p> Django</p>
+          </Col>
+        </Row>
+      </Container>
+    </React.Fragment>
   );
 };
 
