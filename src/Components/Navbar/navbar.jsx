564f1854983a30537b41b7568d4dd34a8cf80ffa
CyrusWarner <cyruswarner27@gmail.com>
2021-08-04 15:48:37 -0500

    Deploy react app to gtihub pages

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -6,12 +6,12 @@ const Navigationbar = () => {
         <div>
         <Navbar bg="dark" variant="dark">
     <Container>
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="me-auto">
+    <Navbar.Brand>Cyrus The Developer</Navbar.Brand>
+    {/* <Nav className="me-auto">
       <Nav.Link href="#home">Home</Nav.Link>
       <Nav.Link href="#About me">About Me</Nav.Link>
       <Nav.Link href="#pricing"></Nav.Link>
-    </Nav>
+    </Nav> */}
     </Container>
   </Navbar>
   </div>
