98454562f4762c5f7e03a5a28b51c946601f94ba
CyrusWarner <cyruswarner27@gmail.com>
2021-08-04 21:42:54 -0500

    Added mobile responsive navbar and edited title of index html.

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -4,16 +4,18 @@ import { Navbar, Container, Nav } from 'react-bootstrap';
 const Navigationbar = () => {
     return (
         <div>
-        <Navbar bg="dark" variant="dark">
-    <Container>
-    <Navbar.Brand>Cyrus The Developer</Navbar.Brand>
-    {/* <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#About me">About Me</Nav.Link>
-      <Nav.Link href="#pricing"></Nav.Link>
-    </Nav> */}
-    </Container>
-  </Navbar>
+<Navbar bg="dark" variant="dark" expand="lg">
+  <Container>
+    <Navbar.Brand href="#home">Cyrus The Developer</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">Link</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
   </div>
     )
 }
