4b65f7f16cfeff1f87293bc84c471deda0bb7412
CyrusWarner <cyruswarner27@gmail.com>
2021-08-04 21:49:42 -0500

    Commented out navbar hamburgermenu until links are ready to be put in component.

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -7,13 +7,13 @@ const Navigationbar = () => {
 <Navbar bg="dark" variant="dark" expand="lg">
   <Container>
     <Navbar.Brand href="#home">Cyrus The Developer</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    {/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
         <Nav.Link href="#home">Home</Nav.Link>
         <Nav.Link href="#link">Link</Nav.Link>
       </Nav>
-    </Navbar.Collapse>
+    </Navbar.Collapse> */}
   </Container>
 </Navbar>
   </div>
