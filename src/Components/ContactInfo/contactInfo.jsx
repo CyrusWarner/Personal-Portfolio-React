fed3e0dca1310e57c185438487f78f4ec7e0da06
CyrusWarner <cyruswarner27@gmail.com>
2021-09-02 13:49:59 -0500

    Added scroll to top icon and added icons for social media to contact section.

--- src/Components/ContactInfo/contactInfo.jsx
+++ src/Components/ContactInfo/contactInfo.jsx
@@ -2,8 +2,25 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import ContactEmailFormModal from '../ContactEmailFormModal/contactEmailFormModal';
 import './contactInfo.css'
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import {IoIosArrowUp} from "react-icons/io"
+import { Link, animateScroll as scroll } from "react-scroll";
+import { motion } from 'framer-motion';
 
 const ContactInfo = () => {
+    const variants = {
+        hover: {
+            scale: 1.1,
+            transition: {
+                duration:0.5,
+                yoyo: Infinity
+            }
+        }
+    }
+
+    const scrollToTop = () => {
+        scroll.scrollToTop();
+    }
     return(
         <div id="contactInfo">
         <Container className="Contact">
@@ -18,10 +35,29 @@ const ContactInfo = () => {
             <Row>
                 <Col sm={8}>
                     <ContactEmailFormModal />
-                    <div style={{color: "#f2f5f7"}} className="hoverDiv mt-2">Phone:<a className="phone" href="tel:1-319-457-1202">1-319-457-1202</a></div>
+                    <div style={{color: "#f2f5f7"}} className="hoverDiv mt-2 mb-2">Phone:<a className="phone" href="tel:1-319-457-1202">1-319-457-1202</a></div>
+                    <a
+              href="https://www.linkedin.com/in/cyrus-warner-687311215/"
+              target="_blank"
+            >
+              <FaLinkedin  className="mb-2" size="3rem" />
+            </a>
+            <a href="https://github.com/CyrusWarner" target="_blank">
+              <FaGithub className="ms-3 mb-2" color="white" size="3rem" />
+            </a>
                 </Col>
                 <Col sm={4}></Col>
             </Row>
+            <Container className="d-flex justify-content-center">
+                <motion.div
+                variants={variants}
+                whileHover="hover"
+                >
+                <Link onClick={scrollToTop}>
+                <IoIosArrowUp className="mb-5" cursor="pointer" size="4rem"  color="white"/>
+                </Link>
+                </motion.div>
+            </Container>
         </Container>
         </div>
     )
--- src/Constants/constants.jsx
+++ src/Constants/constants.jsx
@@ -1,62 +1,68 @@
-import FlashcardImage from '../Images/Flashcard.png'
-import MostWantedImage from '../Images/Most-Wanted.png'
-import TrashCollectorImage from '../Images/trash-collector.png'
-import YoutubeCloneImage from '../Images/Youtube-Clone.png'
-import NorthcottImage from '../Images/Northcott.png'
-import StarWarsEcommerceImage from '../Images/StarWarsEcommerce.png'
-import BugTrackerImage from '../Images/BugTracker.png'
+import FlashcardImage from "../Images/Flashcard.png";
+import MostWantedImage from "../Images/Most-Wanted.png";
+import TrashCollectorImage from "../Images/trash-collector.png";
+import YoutubeCloneImage from "../Images/Youtube-Clone.png";
+import NorthcottImage from "../Images/Northcott.png";
+import StarWarsEcommerceImage from "../Images/StarWarsEcommerce.png";
+import BugTrackerImage from "../Images/BugTracker.png";
 
-    const projects = [
-    {
-        id: 0,
-        title: 'Most-Wanted',
-        description: 'A project that can be used to search for "prisoners" within a data set. This project was built using Javascript, HTML, and CSS',
-        image: MostWantedImage,
-        source: 'https://github.com/CyrusWarner/Most-Wanted',
-    },
-    {
-        id: 1,
-        title: 'Flashcards',
-        description: 'A project using React for the Front-End and Django for the Back-End. Allows a user to create a collection and add flashcards for projects with a variety of animations and features',
-        image: FlashcardImage,
-        source: 'https://github.com/CyrusWarner/Flashcard-Frontend',
-    },
-    {
-        id: 2,
-        title: 'Trash-Collector',
-        description: 'A project built using Django for the Front-End and Back-End. Allows a user to register as an employee or register as a customer and allows for users to schedule pickups and pay using the Paypal API and allows users to confirm pickups ',
-        image: TrashCollectorImage,
-        source: 'https://github.com/Jamesburgess44/Trashcollector',
-    },
-    {
-        id: 3,
-        title: 'Youtube-Clone',
-        description: 'A project built using React for the Front-End and Django for the Back-End. Allows a user to add comments, replies, and search for videos',
-        image: YoutubeCloneImage,
-        source: 'https://github.com/carter-willey/Youtube-Clone-Frontend',
-    },
-    {
-        id: 3,
-        title: 'Northcott Neighborhood House',
-        description: 'This is a website that I helped design multiple pages on. Personally I developed the Juneteenth and Covid Vaccine pages of this website',
-        image: NorthcottImage,
-        source: 'https://northcotthouse.org/juneteenth-2021/',
-    },
-    {
-        id: 4,
-        title: 'Star Wars Ecommerce',
-        description: 'This was my group capstone at devCodeCamp and is a star wars themed ecommerce website. This ecommerce website was created using React for the Front-End and C# for the Back-End',
-        image: StarWarsEcommerceImage,
-        source: 'https://github.com/CyrusWarner/Ecommerce-Front-End'
-    },
-    {
-        id: 5,
-        title: 'Bug Tracker',
-        description: 'This is my solo capstone at devCodeCamp and is a Bug Tracker. Most developers have worked with a Bug Tracker, so i decided to create my own version of a Bug Tracker to test what I learned at devCodeCamp',
-        image: BugTrackerImage,
-        source: 'https://github.com/CyrusWarner/Bug-Tracker-Front-End'
+const projects = [
+  {
+    id: 1,
+    title: "Bug Tracker",
+    description:
+      "This is my solo capstone at devCodeCamp and is a Bug Tracker. Most developers have worked with a Bug Tracker, so i decided to create my own version of a Bug Tracker to test what I learned at devCodeCamp",
+    image: BugTrackerImage,
+    source: "https://github.com/CyrusWarner/Bug-Tracker-Front-End",
+  },
+  {
+    id: 2,
+    title: "Star Wars Ecommerce",
+    description:
+      "This was my group capstone at devCodeCamp and is a star wars themed ecommerce website. This ecommerce website was created using React for the Front-End and C# for the Back-End",
+    image: StarWarsEcommerceImage,
+    source: "https://github.com/CyrusWarner/Ecommerce-Front-End",
+  },
+  {
+    id: 3,
+    title: "Most-Wanted",
+    description:
+      'A project that can be used to search for "prisoners" within a data set. This project was built using Javascript, HTML, and CSS',
+    image: MostWantedImage,
+    source: "https://github.com/CyrusWarner/Most-Wanted",
+  },
+  {
+    id: 4,
+    title: "Flashcards",
+    description:
+      "A project using React for the Front-End and Django for the Back-End. Allows a user to create a collection and add flashcards for projects with a variety of animations and features",
+    image: FlashcardImage,
+    source: "https://github.com/CyrusWarner/Flashcard-Frontend",
+  },
+  {
+    id: 5,
+    title: "Trash-Collector",
+    description:
+      "A project built using Django for the Front-End and Back-End. Allows a user to register as an employee or register as a customer and allows for users to schedule pickups and pay using the Paypal API and allows users to confirm pickups ",
+    image: TrashCollectorImage,
+    source: "https://github.com/Jamesburgess44/Trashcollector",
+  },
+  {
+    id: 6,
+    title: "Youtube-Clone",
+    description:
+      "A project built using React for the Front-End and Django for the Back-End. Allows a user to add comments, replies, and search for videos",
+    image: YoutubeCloneImage,
+    source: "https://github.com/carter-willey/Youtube-Clone-Frontend",
+  },
+  {
+    id: 7,
+    title: "Northcott Neighborhood House",
+    description:
+      "This is a website that I helped design multiple pages on. Personally I developed the Juneteenth and Covid Vaccine pages of this website",
+    image: NorthcottImage,
+    source: "https://northcotthouse.org/juneteenth-2021/",
+  },
+];
 
-    }
-]
-
-export default projects
+export default projects;
