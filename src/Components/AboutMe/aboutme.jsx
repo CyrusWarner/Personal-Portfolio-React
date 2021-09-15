e4c067bb948fc21b3433e34203d858d52e02e175
CyrusWarner <cyruswarner27@gmail.com>
2021-09-15 17:20:18 -0500

    Updated the aboutMe component.

--- src/Components/AboutMe/aboutme.jsx
+++ src/Components/AboutMe/aboutme.jsx
@@ -9,20 +9,19 @@ const AboutMe = () => {
           {" "}
           <p  style={{color: "#f2f5f7"}} id="aboutMe">
             Currently, I am living in Iowa. As of right now, I am currently a
-            student learning programming languages. In my free time, I work on
-            websites and I practice my coding skills. Becoming a software
-            developer is what I believed to be my future career. I graduated
+            graduate from devCodeCamp, enhancing my skills in several programming languages. On my free time, I work on
+            websites and I practice my coding skills. Becoming a Software
+            Developer is what I believe to be my future career. I graduated
             from Southeastern Community College and decided that I would attend
-            DevCodeCamp in hopes of learning things about software development I
+            devCodeCamp in hopes of learning new things about software development I
             was unable to learn in college. As of right now, I am studying
-            Javascript, Python, and C#. In my future I see myself learning
-            multiple different languages to progress throughout my career. I
-            also travel throughout the country, the two places I traveled to
-            recently are El Paso Texas, and Breckenridge Colorado.
+            Typescript, React, and C#. In my future I see myself learning
+            multiple different programming languages and frameworks to progress throughout my career. I
+            also travel throughout the country, the three places I traveled to
+            recently are El Paso Texas, Breckenridge Colorado, and Jamaica.
           </p>
         </Col>
         <Col>
-
         </Col>
       </Row>
     </Container>
